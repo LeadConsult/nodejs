d7efb231cafb414431e2108ee5cbafb2004ced56
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-09-02 21:46:35 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,152 @@
+const http = require('http');
+const puppeteer = require('puppeteer');
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/') {
+        // Serve the HTML page
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(htmlPage);
+    } else if (req.url === '/scrape') {
+        // Scrape data when the /scrape route is requested
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://trends24.in/nigeria/');
+
+            // Scrape data here...
+            const scrapedData = await page.evaluate(() => {
+                const trendElements = document.querySelectorAll('.trend-card');
+                const trendList = [];
+            
+                trendElements.forEach(trendElement => {
+                    const olElement = trendElement.querySelector('ol.trend-card__list');
+                    const trendItems = olElement.querySelectorAll('li');
+            
+                    const trendInfo = [];
+            
+                    trendItems.forEach(trendItem => {
+                        const anchorElement = trendItem.querySelector('a');
+                        const tweetCountElement = trendItem.querySelector('span.tweet-count');
+            
+                        if (anchorElement && tweetCountElement) {
+                            const trendText = anchorElement.textContent;
+                            const tweetCount = tweetCountElement.textContent;
+                            trendInfo.push({ trendText, tweetCount });
+                        }
+                    });
+            
+                    trendList.push(trendInfo);
+                });
+            
+                return trendList;
+            });
+            
+
+            await browser.close();
+
+            // Send the scraped data as a JSON response
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(scrapedData, null, 2));
+        } catch (error) {
+            // Handle errors and send an error response
+            console.error(error);
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Error scraping data' }));
+        }
+    } else {
+        // Handle other routes if needed
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+const htmlPage = `<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+    <button id="scrapeButton">Scrape Data</button>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            <div id="result2">Result 2</div>
+        </div>
+        <div class="col-md-2">
+            <div id="result">Result 1</div>
+        </div>
+    </div>
+</div>
+
+    <script>
+        document.getElementById("scrapeButton").addEventListener("click", () => {
+            // When the button is clicked, send a request to initiate scraping
+            document.getElementById("result").innerText = "Fetching...";
+            fetch("/scrape")
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        document.getElementById("result").innerText = 'Error: ' + data.error;
+                    } else {
+                        // document.getElementById("result").innerText = JSON.stringify(data, null, 2);
+                        console.log(JSON.stringify(data, null, 2));
+
+                    // /////////////////
+                    // Assuming 'data' is an array of arrays containing objects
+                    let formattedDataArray = [];
+                    let formattedDataArray2 = [];
+
+                    data.forEach(innerArray => {
+                        // Convert tweet counts to integers
+                        innerArray.forEach(item => {
+                            item.tweetCount = parseInt(item.tweetCount);
+                        });
+
+                        // Sort the inner array by tweet count in descending order
+                        innerArray.sort((a, b) => b.tweetCount - a.tweetCount);
+
+                        // Create an array to store formatted data
+                        const formattedData = innerArray.map(function(item) {
+                            return item.trendText + " " + item.tweetCount + "K";
+                        });
+                        const formattedData2 = innerArray.map(function(item) {
+                            return item.trendText;
+                        });
+
+                        // Concatenate the formatted data to the accumulator array
+                        formattedDataArray = formattedDataArray.concat(formattedData);
+                        formattedDataArray2 = formattedDataArray2.concat(formattedData2);
+                    });
+                    // Join all formatted data with line breaks
+                    const formattedDataString = formattedDataArray.join('<br>');
+                    const formattedDataString2 = formattedDataArray2.join('<br>');
+
+                    // Output the sorted data to an HTML element with the ID "result"
+                    document.getElementById("result").innerHTML = formattedDataString;
+                    document.getElementById("result2").innerHTML = formattedDataString2;
+
+
+                    // /////////////////
+                    }
+                })
+                .catch(error => {
+                    document.getElementById("result").innerText = 'Fetch error: ' + error.message;
+                });
+        });
+    </script>
+    <!-- Bootstrap JavaScript (optional) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>`;
